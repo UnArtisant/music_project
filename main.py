fbc892865e4f41e895ba9f87197fbc5c7021adf3
Unaxe <74629449+Unaxe@users.noreply.github.com>
2021-01-04 21:57:48 +0100

    Correct

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ Fichier main
 from src.Service.InterfaceService import interfaceService
 
 if __name__ == "__main__":
-    file = input("De quel fichier voulez-vous vous traiter les partitions ?\n ")
-    interface = interfaceService(file)
+    interface = interfaceService("partitions.txt")
     interface.interface()
